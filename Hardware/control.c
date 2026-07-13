53cc86208d9dee6c41a75f4dc0f0810722235aa1
Lanlan <2784193144@qq.com>
2026-07-13 16:08:12 +0800

    添加pitch轴软件限位，防止缠线

--- Hardware/control.c
+++ Hardware/control.c
@@ -3,8 +3,113 @@
 #include "config.h"
 #include "vofa_debug.h"
 
-#define GIMBAL_HOME_YAW_ANGLE 0.0f
-#define GIMBAL_HOME_PITCH_ANGLE 0.0f
+// 弧度转角度系数，180/pi
+#define GIMBAL_RAD_TO_DEG 57.2957795f
+
+static float gimbal_pitch_up_limit_deg = GIMBAL_PITCH_DEFAULT_UP_LIMIT_DEG;
+static float gimbal_pitch_down_limit_deg = GIMBAL_PITCH_DEFAULT_DOWN_LIMIT_DEG;
+
+/**
+ * @brief 求浮点数绝对值
+ * @param value 输入值
+ * @return 绝对值
+ */
+static float Gimbal_Abs(float value) {
+    return (value < 0.0f) ? -value : value;
+}
+
+/**
+ * @brief 限制浮点数范围
+ * @param value 输入值
+ * @param min 最小值
+ * @param max 最大值
+ * @return 限幅后的值
+ */
+static float Gimbal_Clamp(float value, float min, float max) {
+    if (value < min) return min;
+    if (value > max) return max;
+    return value;
+}
+
+/**
+ * @brief 将角度限制到[-pi, pi)范围
+ * @param angle 输入角度，单位rad
+ * @return 限制后的角度，单位rad
+ */
+static float Gimbal_WrapRad(float angle) {
+    while (angle >= QD4310_PI) {
+        angle -= QD4310_TWO_PI;
+    }
+    while (angle < -QD4310_PI) {
+        angle += QD4310_TWO_PI;
+    }
+    return angle;
+}
+
+/**
+ * @brief 获取pitch轴相对0点的角度
+ * @return pitch轴相对GIMBAL_HOME_PITCH_ANGLE的角度，向上为正，单位deg
+ */
+float Gimbal_GetPitchRelativeAngleDeg(void) {
+    float angle = Gimbal_WrapRad(motor_pitch_handle.angle - GIMBAL_HOME_PITCH_ANGLE);
+    return angle * GIMBAL_RAD_TO_DEG * GIMBAL_PITCH_POSITIVE_SPEED_IS_UP;   // 把弧度转换成角度
+}
+
+/**
+ * @brief 设置pitch轴软限位角度
+ * @param up_limit_deg 0点上方允许转动角度，单位deg
+ * @param down_limit_deg 0点下方允许转动角度，单位deg
+ * @return 1: 设置成功 0: 参数错误
+ */
+uint8_t Gimbal_SetPitchLimitsDeg(float up_limit_deg, float down_limit_deg) {
+    if (up_limit_deg < 0.0f || down_limit_deg < 0.0f) {
+        return 0;
+    }
+
+    gimbal_pitch_up_limit_deg = Gimbal_Clamp(up_limit_deg, 0.0f, GIMBAL_PITCH_MAX_LIMIT_DEG);
+    gimbal_pitch_down_limit_deg = Gimbal_Clamp(down_limit_deg, 0.0f, GIMBAL_PITCH_MAX_LIMIT_DEG);
+
+    return 1;
+}
+
+/**
+ * @brief 根据pitch轴当前位置限制目标转速，防止超过软限位
+ * @param speed 视觉PID输出的pitch目标转速，单位rpm
+ * @return 限位后的pitch目标转速，单位rpm
+ */
+static float Gimbal_LimitPitchSpeed(float speed) {
+    float angle_deg = Gimbal_GetPitchRelativeAngleDeg();
+    float up_speed = speed * GIMBAL_PITCH_POSITIVE_SPEED_IS_UP;
+    float speed_abs = Gimbal_Abs(up_speed);
+    float remaining_deg;  // 当前方向距离软限位还剩余的角度，单位deg
+    float allowed_speed;  // 根据剩余角度计算出的允许最大速度，单位rpm
+
+    speed_abs = Gimbal_Clamp(speed_abs, 0.0f, GIMBAL_PITCH_MAX_SPEED_RPM);
+
+    if (up_speed > 0.0f) {
+        remaining_deg = gimbal_pitch_up_limit_deg - angle_deg;
+        if (remaining_deg <= GIMBAL_PITCH_LIMIT_STOP_MARGIN_DEG) {
+            return 0.0f;
+        }
+        allowed_speed = (remaining_deg - GIMBAL_PITCH_LIMIT_STOP_MARGIN_DEG) *
+                        GIMBAL_PITCH_LIMIT_SPEED_GAIN;
+        speed_abs = Gimbal_Clamp(speed_abs, 0.0f, allowed_speed);
+        return speed_abs * GIMBAL_PITCH_POSITIVE_SPEED_IS_UP;
+    }
+
+    if (up_speed < 0.0f) {
+        remaining_deg = gimbal_pitch_down_limit_deg + angle_deg;
+        if (remaining_deg <= GIMBAL_PITCH_LIMIT_STOP_MARGIN_DEG) {
+            return 0.0f;
+        }
+        allowed_speed = (remaining_deg - GIMBAL_PITCH_LIMIT_STOP_MARGIN_DEG) *
+                        GIMBAL_PITCH_LIMIT_SPEED_GAIN;
+        speed_abs = Gimbal_Clamp(speed_abs, 0.0f, allowed_speed);
+        return -speed_abs * GIMBAL_PITCH_POSITIVE_SPEED_IS_UP;
+    }
+
+    return 0.0f;
+}
 
 /**
  * @brief 云台控制初始化函数
@@ -22,6 +127,10 @@ void Control_Init(void) {
  * @param speed 转速，单位rpm
  */
 void Set_Motor_Speed(QD4310_t *motor, float speed) {
+    if (motor == &motor_pitch_handle) {
+        // pitch轴不允许720度连续旋转，先经过软限位再下发速度
+        speed = Gimbal_LimitPitchSpeed(speed);
+    }
     // if (speed <= 5 && speed >= -5) {
     //     QD4310_SetLowSpeed(motor, speed); // 低速模式
     // } else {
@@ -65,4 +174,3 @@ void Gimbal_Track(Maixcam_Data_t maixcam_data) {
     Set_Motor_Speed(&motor_yaw_handle, speed_yaw);
     Set_Motor_Speed(&motor_pitch_handle, speed_pitch);
 }
-
--- Hardware/control.h
+++ Hardware/control.h
@@ -3,6 +3,24 @@
 
 #include "QD4310.h"
 
+// 云台回零角度，单位rad；pitch软限位以GIMBAL_HOME_PITCH_ANGLE为0点
+#define GIMBAL_HOME_YAW_ANGLE                 0.0f
+#define GIMBAL_HOME_PITCH_ANGLE               0.0f
+
+// pitch轴默认上/下限位角度，单位deg；可通过Gimbal_SetPitchLimitsDeg动态修改
+#define GIMBAL_PITCH_DEFAULT_UP_LIMIT_DEG     30.0f
+#define GIMBAL_PITCH_DEFAULT_DOWN_LIMIT_DEG   30.0f
+#define GIMBAL_PITCH_MAX_LIMIT_DEG           170.0f
+
+// 如果pitch正速度会让镜头向下运动，则将该宏改为-1.0f
+#define GIMBAL_PITCH_POSITIVE_SPEED_IS_UP      1.0f
+// 距离软限位小于该角度时停止继续向外转动，单位deg
+#define GIMBAL_PITCH_LIMIT_STOP_MARGIN_DEG     1.0f
+// 靠近软限位时的降速系数，单位rpm/deg
+#define GIMBAL_PITCH_LIMIT_SPEED_GAIN          3.0f
+// pitch轴软件限速，防止视觉PID输出过大速度，单位rpm
+#define GIMBAL_PITCH_MAX_SPEED_RPM           180.0f
+
 /* MaixCam数据结构体 */
 typedef struct {
     float pixel_dx;         // pixel_dx MaixCam目标X坐标 - 屏幕中心X坐标
@@ -14,5 +32,9 @@ void Control_Init(void);
 void Gimbal_GotoZero(void);
 void Set_Motor_Speed(QD4310_t *motor, float speed);
 void Gimbal_Track(Maixcam_Data_t maixcam_data);
+// 修改pitch轴相对0点的上/下限位角度，单位deg
+uint8_t Gimbal_SetPitchLimitsDeg(float up_limit_deg, float down_limit_deg);
+// 获取pitch轴相对0点的角度，向上为正，单位deg
+float Gimbal_GetPitchRelativeAngleDeg(void);
 
 #endif
